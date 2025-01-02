9a80abbd0f7e679107ff22dbede6408de778e158
dsad233 <ksl1421@naver.com>
2025-01-02 22:14:49 +0900

    refactor : crawling 코드 수정

--- src/youtube-service/youtube-service.service.ts
+++ src/youtube-service/youtube-service.service.ts
@@ -9,70 +9,10 @@ import puppeteer from "puppeteer";
 @Injectable()
 export class YoutubeServiceService {
   constructor(private readonly configService : ConfigService){}
-  private readonly apiKey = this.configService.get<string>(ENV_Youtube_ApiKey);
-  private readonly apiUrl_Video = this.configService.get<string>(ENV_Youtube_ApiUrl);
-  private readonly apiUrl_Search = this.configService.get<string>(ENV_Youtube_Search);
-  private readonly apiUrl_Channel = this.configService.get<string>(ENV_Youtube_Channel);
-
-
-  // // 동영상 정보 검색 (동영상 Id만 입력해서 검색)
-  // async getVideoDetails(videoId: string) {
-  //   const url = `${this.apiUrl_Video}?part=snippet,contentDetails,statistics&id=${videoId}&key=${this.apiKey}`;
-
-  //   const response = await axios.get(url);
-  //   const videos = response.data.items;
-
-  //   if (videos && videos.length === 0) {
-  //     throw new NotFoundException('채널이 존재하지 않습니다.');
-  //   }
-    
-  //   return videos;
-  // }
-
-  // // 채널 정보 검색 (channelId로만 검색이 가능)
-  // async getChannel(channelId: string, maxResults: number = 50) {
-  //   const url = `${this.apiUrl_Search}?part=snippet&channelId=${channelId}&maxResults=${maxResults}&key=${this.apiKey}`;
-
-  //   const response = await axios.get(url);
-  //   const channels = response.data.items;
-
-  //   if (channels && channels.length === 0) {
-  //     throw new NotFoundException('채널이 존재하지 않습니다.');
-  //   }
-
-  //   return channels;
-  // }
-
-  // // 유튜브 검색
-  // async getSearch(title: string, maxResults: number = 50) {
-  //   const encoded = encodeURIComponent(title);
-  //   const url = `${this.apiUrl_Search}?part=snippet&q=${encoded}&maxResults=${maxResults}&key=${this.apiKey}`;
-
-  //   const response = await axios.get(url);
-  //   const searchs = response.data.items;
-
-  //   if (searchs && searchs.length === 0) {
-  //     throw new NotFoundException('채널이 존재하지 않습니다.');
-  //   }
-
-  //   return searchs;
-  // }
-
-  // // 채널이름 검색(@제외 채널이름 검색)
-  // async getChannelName(channelName: string) {
-  //   const url = `${this.apiUrl_Channel}?part=snippet&forHandle=@${channelName}&key=${this.apiKey}`;
-
-  //   const response = await axios.get(url);
-  //   const channels = response.data.items;
-
-  //   if (channels && channels.length === 0) {
-  //     throw new NotFoundException('채널이 존재하지 않습니다.');
-  //   }
-
-  //   // const result = channels[0].snippet.title;
-    
-  //   return channels;
-  // }
+  // private readonly apiKey = this.configService.get<string>(ENV_Youtube_ApiKey);
+  // private readonly apiUrl_Video = this.configService.get<string>(ENV_Youtube_ApiUrl);
+  // private readonly apiUrl_Search = this.configService.get<string>(ENV_Youtube_Search);
+  // private readonly apiUrl_Channel = this.configService.get<string>(ENV_Youtube_Channel);
 
   // 유튜브 검색(크롤링)
   async searchMusicVideos(query: string) {
@@ -115,19 +55,24 @@ export class YoutubeServiceService {
     const record = encodeURIComponent(query);
 
     const searchUrl = `https://www.youtube.com/watch?v=${record}`;
-    await page.goto(searchUrl);
+    await page.goto(searchUrl, { waitUntil : 'networkidle2' });
 
-    await page.waitForSelector('h3 > a', { timeout : 500 });
+    await page.waitForSelector('#movie_player > div.html5-video-container > video');
     await page.screenshot({ path: 'screenshot.png', fullPage: false });
 
-    const videos = await page.evaluate(() => {
-      const videoElements = Array.from(document.querySelectorAll('html5-video-container'));
-      return videoElements.map(video => {
-        const titleElement = video.querySelector('#video-title');
-        const url = titleElement ? `https://www.youtube.com${titleElement.getAttribute('href')}` : '';
-        const title = titleElement ? titleElement.textContent.trim() : '';
+    console.log(page);
 
-        return { title, url };
+    const videos = await page.evaluate(() => {
+      const videoElements = Array.from(document.querySelectorAll('#primary-inner'));
+      return videoElements.map(video => {
+        const title = video.querySelector('#title > h1 > yt-formatted-string').textContent.trim();
+        const tagName = video.querySelector("#text > a").textContent.trim();
+        const videoInfo = video.querySelector("#attributed-snippet-text > span > span").textContent.trim();
+        const createDate = video.querySelector("#info > span:nth-child(3)").textContent.trim();
+        const count = video.querySelector("#info > span:nth-child(1)").textContent.trim();
+        const url = window.location.href;
+
+        return { title, url, tagName, createDate, count, videoInfo };
       });
     });
 
