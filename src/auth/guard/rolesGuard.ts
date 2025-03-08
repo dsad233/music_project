96b0ee2c5d902947f2a8b831b4e0b62526c3dd28
dsad233 <ksl1421@naver.com>
2025-03-09 00:08:58 +0900

    Fix : rolesguard await 추가

--- src/auth/guard/rolesGuard.ts
+++ src/auth/guard/rolesGuard.ts
@@ -13,7 +13,7 @@ export class RolesGuard implements CanActivate {
             return true;
         }
         
-        const request = context.switchToHttp().getRequest();
+        const request = await context.switchToHttp().getRequest();
         const user = request.user;
 
         if(!user){
