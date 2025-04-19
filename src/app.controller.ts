9a559762fc6d2aa74c0a07ab8564d78ae63c164f
dsad233 <ksl1421@naver.com>
2025-04-19 22:56:17 +0900

    chore: grafana 설정 관련 패키지 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as client from 'prom-client';
+import { Response } from 'express';
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
 
 @Controller()
 export class AppController {
@@ -8,5 +13,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/metrics')
+  async prometheHealth(@Res() res: Response){
+    res.setHeader('Content-Type', register.contentType);
+    const metrics = await register.metrics();
+    res.end(metrics);
   }
 }
