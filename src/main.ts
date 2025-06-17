cd537d838a30ebfc76fae19806a2b3f900167db8
dsad233 <ksl1421@naver.com>
2025-06-17 20:20:22 +0900

    feat: loki, tempo compose 추가

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ErrorException } from './middleware/errorException';
 import { LoggerMiddleware } from './middleware/logger';
+import { winstonLogger } from './common/logger/winston';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+    bufferLogs: true,
+  });
   app.enableCors({
     origin: ['http://localhost:3111'],
     credentials: true,
